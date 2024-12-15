c024bea1797fc9da7da6ec128fe2f74801584b31
Thepropollo <ramonb634@gmail.com>
2024-12-15 09:31:28 -0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import HomePage from './components/HomePage';
+import Login from './components/Login';
+
+const App = () => {
+    return (
+        <Router>
+            <Routes>
+                <Route path="/" element={<HomePage />} />
+                <Route path="/login" element={<Login />} />
+            </Routes>
+        </Router>
+    );
+};
+
+export default App;
